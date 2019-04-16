09b9f5f293e45c6bacaa04aa06b47b6266f46bdd
sintan1071 <297085213@qq.com>
2019-04-16 18:44:05 +0800

    编写install脚本

---
+++ goversionmanager.sh
@@ -0,0 +1,86 @@
+#!/bin/sh
+
+#--------------------------------------------
+#-This is go version manager script for Golang
+#@@@ add by SinTan1071
+#--------------------------------------------
+
+PARENTDIR="/home/sintan1071"
+ROOTDIR="/usr/local"
+BINDIR="/usr/local/bin"
+VAR=
+
+[ -f /etc/init.d/functions ] && {
+    . /etc/init.d/functions
+} || {
+    . $PARENTDIR/shell/functions
+}
+
+useGoEnv() {
+    { nohup sh $GOLANDSH \
+	1>$JETLOG \
+   	2>&1 &
+    } 
+    return 0
+}
+
+listGoEnv() {
+    { nohup sh $PYCHARMSH \
+	1>$JETLOG \
+        2>&1 &
+    }
+    return 0
+}
+
+installGoEnv() {
+    if [ ! -n $VAR ]; then
+        echo "you should input the golang version you want to install"
+        return 0
+    fi
+    if [ -d $ROOTDIR/go$VAR ] && [ -f $BINDIR/go$VAR ]; then
+        echo "you already have the go$VAR installed"
+        return 0
+    fi
+    echo "installing go$VAR...   "
+    {
+        wget --quiet --no-check-certificate https://dl.google.com/go/go$VAR.linux-amd64.tar.gz && \
+        tar -C $ROOTDIR -xzf go$VAR.linux-amd64.tar.gz && \
+        mv $ROOTDIR/go $ROOTDIR/go$VAR && \
+        ln -s $ROOTDIR/go$VAR/bin/go $BINDIR/go$VAR
+    } &
+    POINTS='                                                  '
+    PROCESS=$((1))
+    while [ true ]; do
+        echo -ne "\033[42;1m${POINTS:0:$(( $PROCESS/2 ))}\033[0m${POINTS:$(( $PROCESS/2 ))}$PROCESS%\r"
+        if [ $PROCESS -eq  $((100)) ];then
+            break
+        fi
+        if [ -d $ROOTDIR/go$VAR ] && [ -f $BINDIR/go$VAR ];then
+            rm go$VAR.linux-amd64.tar.gz
+            PROCESS=$((100))
+        fi
+        if [ ! -d $ROOTDIR/go$VAR ] && [ $PROCESS -lt 73 ];then
+            PROCESS=$(( $PROCESS + 1 ))
+        fi
+        sleep 3
+    done
+    return 0
+}
+
+VAR=$2
+case "$1" in
+    use)
+        useGoEnv || exit 1
+        ;;
+    ls)
+        listGoEnv || exit 1
+        ;;
+    install)
+        installGoEnv || exit 1
+        ;;
+    *)
+        echo "Usage: $0 {use|ls|install}"
+        exit 1
+        ;;
+esac
+
