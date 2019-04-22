cbb66f982f479ee1f2797cc7598b799faa0125b0
sintan1071 <297085213@qq.com>
2019-04-22 19:09:52 +0800

    编写安装脚本

---
+++ .install.sh
@@ -0,0 +1,30 @@
+main() {
+    USERPATH=~
+    SHCMD=
+    echo "Installing Go Version Manager..." 
+    command -v git >/dev/null 2>&1 || {
+        echo "Error: git is not installed"
+        exit 1
+    }
+    env git clone --depth=1 https://github.com/sintan1071/govm.git "$USERPATH/.govm" || {
+        echo "Error: git clone of govm repo failed"
+        exit 1
+    }
+    string=$SHELL  
+    array=(${SHELL//"/"/ }) 
+    i=$((0)) 
+    for var in ${array[@]}
+    do
+        i=$(($i+1))
+        if [ $var = bin ];then
+            SHCMD=${array[$i]}
+        fi
+    done 
+    echo "##############################" >> $USERPATH/."$SHCMD"rc
+    echo "##    Go Version Manager    ##" >> $USERPATH/."$SHCMD"rc
+    echo "##############################" >> $USERPATH/."$SHCMD"rc
+    echo "alias govm=bash $USERPATH/.govm/goversionmanager.sh"
+    source $USERPATH/."$SHCMD"rc
+}
+
+main
