7c586806046d7ed03aaf50c9813ed0b55e9bed25
sintan1071 <297085213@qq.com>
2019-04-22 17:36:37 +0800

    编写go version list命令逻辑

--- goversionmanager.sh
+++ goversionmanager.sh
@@ -2,26 +2,48 @@
 
 #--------------------------------------------
 #-This is a version manager script for Golang
-#@@@ add by SinTan1071
+#@@ by SinTan1071
 #--------------------------------------------
+OS=$(uname -a | awk '{print $1}')
+case "$OS" in
+    Linux)
+        ARCH=$(dpkg --print-architecture)
+        ;;
+    FreeBSD)
+        ARCH=$(dpkg --print-architecture)
+        ;;
+    Darwin)
+        ARCH=amd64
+        ;;
+    *)
+        echo "Sorry, Your OS is not available for this script !"
+        exit 1
+        ;;
+esac
 
 VAR=$2
-PARENTDIR="/home/sintan1071"
+WORKDIR=$(pwd)
 ROOTDIR="/usr/local"
 BINDIR="/usr/local/bin"
-DOWNLOADURL="https://dl.google.com/go/go$VAR.linux-amd64.tar.gz"
-
-[ -f /etc/init.d/functions ] && {
-    . /etc/init.d/functions
-} || {
-    . $PARENTDIR/shell/functions
-}
+TMPDIR=$WORKDIR/.tmp
+DOWNLOADURL="https://dl.google.com/go/go$VAR.$OS-$ARCH.tar.gz"
 
 useGoEnv() {
     return 0
 }
 
 listGoEnv() {
+    NOWUSING=$(go version | awk '{print $3}')
+    for v in `ls $BINDIR | grep go`
+    do
+        if [ $v != "go" ];then
+            if [ $v = $NOWUSING ];then
+                echo -e "\033[33m$v\033[0m    <--- Using Now"
+            else
+                echo $v
+            fi
+        fi
+    done
     return 0
 }
 
@@ -44,29 +66,29 @@ installGoEnv() {
         echo "no go$VAR is available"
         return 0
     fi
-    if [ -f go$VAR.linux-amd64.tar.gz ];then
+    if [ -f $TMPDIR/go$VAR.$OS-$ARCH.tar.gz ];then
         SERVERLEN=$(curl -sI $DOWNLOADURL | grep content-length | awk '{print $2}')
-        LOCALLEN=$(ls -l go$VAR.linux-amd64.tar.gz | awk '{print $5}')
+        LOCALLEN=$(ls -l $TMPDIR/go$VAR.$OS-$ARCH.tar.gz | awk '{print $5}')
         if [ ${SERVERLEN:0:$((${#SERVERLEN}-1))} = $LOCALLEN ];then
-            tar -C $ROOTDIR -xzf go$VAR.linux-amd64.tar.gz && \
+            tar -C $ROOTDIR -xzf $TMPDIR/go$VAR.$OS-$ARCH.tar.gz && \
             mv $ROOTDIR/go $ROOTDIR/go$VAR && \
             ln -s $ROOTDIR/go$VAR/bin/go $BINDIR/go$VAR
             echo "go$VAR install successfull"
             return 0
         else
-            rm go$VAR.linux-amd64.tar.gz
+            rm $TMPDIR/go$VAR.$OS-$ARCH.tar.gz
         fi
     fi
     echo "installing go$VAR...   "
     {
-        wget --quiet --no-check-certificate $DOWNLOADURL && \
-        tar -C $ROOTDIR -xzf go$VAR.linux-amd64.tar.gz && \
+        wget --quiet --no-check-certificate -P $TMPDIR $DOWNLOADURL && \
+        tar -C $ROOTDIR -xzf $TMPDIR/go$VAR.$OS-$ARCH.tar.gz && \
         mv $ROOTDIR/go $ROOTDIR/go$VAR && \
         ln -s $ROOTDIR/go$VAR/bin/go $BINDIR/go$VAR
     } &
     BARS='                                                  '
     PROCESS=$((1))
-    SLEEPTIME=2
+    SLEEPTIME=3
     while [ true ]; do
         echo -ne "\033[42;1m${BARS:0:$(( $PROCESS/2 ))}\033[0m${BARS:$(( $PROCESS/2 ))}$PROCESS%\r"
         if [ $PROCESS -eq  $((100)) ];then
@@ -74,7 +96,7 @@ installGoEnv() {
             break
         fi
         if [ -d $ROOTDIR/go$VAR ] && [ -f $BINDIR/go$VAR ];then
-            rm go$VAR.linux-amd64.tar.gz
+            rm $TMPDIR/go$VAR.$OS-$ARCH.tar.gz
             PROCESS=$(( $PROCESS + 1 ))
             SLEEPTIME=0.05
         fi
